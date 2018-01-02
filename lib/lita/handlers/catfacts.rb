849d60338b5e48b71c15610f49df612629c858da
Brutalbeard <jceloria@gmail.com>
2018-01-01 22:54:25 -0500

    Update catfacts.rb

--- lib/lita/handlers/catfacts.rb
+++ lib/lita/handlers/catfacts.rb
@@ -6,7 +6,7 @@ module Lita
       route %r{^catfact(|s)$}i, :catfacts, command: true, help: { "catfacts" => "Returns a random fact about cats." }
 
       def catfacts(response)
-        resp = http.get('http://catfacts-api.appspot.com/api/facts?number=1')
+        resp = http.get('https://catfact.ninja/fact')
 
         raise 'RequestFail' unless resp.status == 200
         
