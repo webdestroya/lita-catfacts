dd2ce9079df13c017030e6ada54183b8be3679cc
Brutalbeard <jceloria@gmail.com>
2018-05-14 03:06:09 -0400

    Change to catfact ninja (#2)
    * Update catfacts.rb
    
    * Update catfacts.rb
    
    * Update catfacts.rb

--- lib/lita/handlers/catfacts.rb
+++ lib/lita/handlers/catfacts.rb
@@ -6,16 +6,13 @@ module Lita
       route %r{^catfact(|s)$}i, :catfacts, command: true, help: { "catfacts" => "Returns a random fact about cats." }
 
       def catfacts(response)
-        resp = http.get('http://catfacts-api.appspot.com/api/facts?number=1')
+        resp = http.get('https://catfact.ninja/fact')
 
         raise 'RequestFail' unless resp.status == 200
         
         obj = MultiJson.load(resp.body)
 
-        raise 'ApiError' unless obj['success'] == "true"
-
-
-        response.reply obj['facts'][0]
+        response.reply obj['fact']
 
       rescue
         response.reply "Sorry, I was unable to find any cool facts about cats."
